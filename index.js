752192753e77a1b5dd217e959622617cf1ac0ad1
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-08 12:45:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+const recipes = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(recipes, { [key]: value })
+}
+updateObjectWithKeyAndValue(recipes, prop2, 2)
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+
+destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop', '1')
+
+function deleteFromObjectByKey(object, key) {
+  return delete recipes.prop
+}
+removeFromPlaylist(recipes, 'prop')
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+return delete recipes.prop2
+}
+removeFromPlaylist(recipes, 'prop2')
+
